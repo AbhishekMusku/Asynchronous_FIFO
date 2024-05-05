7ec0e77c60d29d60fdac1cba58226f776adc70f1
AbhishekMusku <163239329+AbhishekMusku@users.noreply.github.com>
2024-05-04 20:35:28 -0700

    Update fifomem.sv

--- fifomem.sv
+++ fifomem.sv
@@ -1,21 +1,23 @@
-module fifomem #(parameter DATASIZE = 8, // Memory data word width
- parameter ADDRSIZE = 8) // Number of mem address bits
- (output [DATASIZE-1:0] rdata,
- input [DATASIZE-1:0] wdata,
- input [ADDRSIZE-1:0] waddr, raddr,
- input wclken, wfull, wclk);
- `ifdef VENDORRAM
- // instantiation of a vendor's dual-port RAM
- vendor_ram mem (.dout(rdata), .din(wdata),
- .waddr(waddr), .raddr(raddr),
- .wclken(wclken),
- .wclken_n(wfull), .clk(wclk));
- `else
- // RTL Verilog memory model
- localparam DEPTH = 1<<ADDRSIZE;
- reg [DATASIZE-1:0] mem [0:DEPTH-1];
- assign rdata = mem[raddr];
- always @(posedge wclk)
- if (wclken && !wfull) mem[waddr] <= wdata;
- `endif
+module fifo_mem #(parameter DEPTH=256, DATA_WIDTH=8, PTR_WIDTH=8) (
+  input wclk, w_en, rclk, r_en,
+  input [PTR_WIDTH:0] b_wptr, b_rptr,
+  input [DATA_WIDTH-1:0] data_in,
+  input full, empty,
+  output reg [DATA_WIDTH-1:0] data_out
+);
+  reg [DATA_WIDTH-1:0] fifo[0:DEPTH-1];
+  
+  always@(posedge wclk) begin
+    if(w_en & !full) begin
+      fifo[b_wptr[PTR_WIDTH-1:0]] <= data_in;
+    end
+  end
+  /*
+  always@(posedge rclk) begin
+    if(r_en & !empty) begin
+      data_out <= fifo[b_rptr[PTR_WIDTH-1:0]];
+    end
+  end
+  */
+  assign data_out = fifo[b_rptr[PTR_WIDTH-1:0]];
 endmodule
